8cd6be4132a50f1bf783625e226ea63648e867df
josejam <joseam.jam.79@gmail.com>
2024-09-19 18:24:19 +0200

    add cambios en ejer-2 Ejercicios-Propuestos-Capitulo-1

--- Curso-Rust/Ejer-Propuestos-Capitulo-1/ejer-2/main.rs
+++ Curso-Rust/Ejer-Propuestos-Capitulo-1/ejer-2/main.rs
@@ -1,3 +1,16 @@
+/*  Ejer 2  Escribir un programa que tome como entrada un número entero y nos indique qué cantidad
+            hay que sumarle para que el resultado sea múltiplo de 7
+
+                Ejemplo:
+                    * A 2 hay que sumarle 5 para que el resultado (2 + 5 = 7) sea múltiplo de 7
+                    * A 13 hay que sumarle 1 para que el resultado (13 + 1 = 14) sea múltiplo
+                        de 7
+            Si proporcionamos el número 2 o el 13, la salida de la aplicación debe ser 5 o 1 respectivamente
+*/
+
+use std::io;
+use std::str::FromStr;
+
 fn main() {
     println!("Hello, world!");
 }
