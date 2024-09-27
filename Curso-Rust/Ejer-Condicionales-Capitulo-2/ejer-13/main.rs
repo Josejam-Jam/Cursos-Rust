1bbd1f90c35cdba5bf610297f964b5dee872c018
josejam <joseam.jam.79@gmail.com>
2024-09-27 20:07:48 +0200

    add mod-Ejer-13-V2 Ejer-Condicionales-Capitulo-2

--- Curso-Rust/Ejer-Condicionales-Capitulo-2/ejer-13/main.rs
+++ Curso-Rust/Ejer-Condicionales-Capitulo-2/ejer-13/main.rs
@@ -10,30 +10,81 @@ use std::io;
 use std::str::FromStr;
 
 fn main() {
-    /* 
-    let hora: i32;
-    let min: i32;
-    let seg: i32;
-    */
-        
-    let prueba = get_increment_hora_tupla(0,0,59);
 
-    print_incrementar_hora_tupla(prueba);
+    let mut hora: i32;
+    let mut min: i32;
+    let mut seg: i32;
+    let result_data: (i32, i32, i32);
+
+        loop {
+
+            println!("\nIntroduce Hora Formato [ 24H ]");
+            let mut entrada1 = String::new();
+
+            io::stdin().read_line(&mut entrada1).expect("Error en la Lectura de datos");
+            hora = i32::from_str(&entrada1.trim()).unwrap();
+
+                if hora >= 1 && hora <= 24 {
+                    break;
+                } else {
+                    println!("\nError la Hora introducida es errónea\n");
+                }
+        }
+
+        loop {
+
+            println!("\nIntroduce Minutos");
+            let mut entrada2 = String::new();
+
+            io::stdin().read_line(&mut entrada2).expect("Error en la Lectura de datos");
+            min = i32::from_str(&entrada2.trim()).unwrap();
+
+                if min >= 0 && min <= 59 {
+                    break;
+                } else {
+                    println!("\nError la Minutos introducida es errónea\n");
+                }
+        }
+
+        loop {
+            println!("\nIntroduce Segundos");
+            let mut entrada3 = String::new();
+
+            io::stdin().read_line(&mut entrada3).expect("Error en la Lectura de datos");
+            seg = i32::from_str(&entrada3.trim()).unwrap();
+
+                if seg >= 0 && seg <= 59 {
+                    break;
+                } else {
+                    println!("\nError la Hora introducida es errónea\n");
+                }
+        }
+    
+        
+    result_data = get_increment_hora_tupla(hora, min, seg);
+    // println!("{}", result_data.to_string());
+
+    print_incrementar_hora_tupla(result_data);
+
+    //get_print_increment_hora(hora, min, seg);
 }
 
 fn get_increment_hora_tupla(mut hora: i32, mut min: i32, mut seg: i32) -> (i32, i32, i32) {
 
-    if seg + 1 == 60 {
-        min += 1;
-        seg = 0;
+        if seg + 1 == 60 {
+            min += 1;
+            seg = 0;
+        } 
+        
+        if min == 60 {
+            hora += 1;
+            min = 0;
 
-    } else if min == 60 {
-        hora += 1;
-        min = 0;
-
-    } else if hora > 23 {
-        hora = 0;
-    } 
+        }
+        
+        if hora > 23 {
+            hora = 0;
+        } 
 
     let result_tupla = (hora, min, seg);
 
@@ -72,23 +123,27 @@ fn print_incrementar_hora_tupla(result_tupla: (i32, i32, i32)) {
     println!("\nHora :\t{}:{}:{}\n", result.0, result.1, result.2);
     */    
 }
-
+/*
 fn get_print_increment_hora(mut hora: i32, mut min: i32, mut seg: i32) {
     
         if seg + 1 == 60 {
             min += 1;
             seg = 0;
-
-        } else if min == 60 {
+        } 
+        
+        if min == 60 {
             hora += 1;
             min = 0;
 
-        } else if hora > 23 {
+        }
+        
+        if hora > 23 {
             hora = 0;
-        } 
+        }
 
     let result_tupla = (hora, min, seg);
 
     print_incrementar_hora_tupla(result_tupla);
 
 }
+*/
