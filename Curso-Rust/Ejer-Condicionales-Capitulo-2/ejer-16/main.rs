a38fad940e8c4272fcb6746ea8d48b1d49711f1e
josejam <joseam.jam.79@gmail.com>
2024-09-30 16:24:52 +0200

    add mod-ejer-16-V2 Ejercicios-Condicionales-Capitulo-2

--- Curso-Rust/Ejer-Condicionales-Capitulo-2/ejer-16/main.rs
+++ Curso-Rust/Ejer-Condicionales-Capitulo-2/ejer-16/main.rs
@@ -22,7 +22,93 @@ fn main() {
                     println!("\nError al Introducir los datos");
                 }
         }
+        
+    /*  VERSION 2 FUNCIÓN */
+    let result_tupla = get_num_en_letras_tupla(num);
 
+    println!("\nNúmero\t{} en Letras:\t{}{}{}\n", num, result_tupla.0, result_tupla.1, result_tupla.2);
+
+    /*  VERSIÓN 1 */    
+    /*let unid: i32;
+    let dec: i32;
+    // let unid: i16;
+    // let dec: i16;
+
+    let mut result_tupla: (String, String, String) = ("".to_string(), "".to_string(), "".to_string());
+
+        if num >= 10 && num <= 15 {
+            
+            result_tupla.0 = match num {
+                10 => "diez".to_string(),
+                11 => "once".to_string(),
+                12 => "doce".to_string(),
+                13 => "trece".to_string(),
+                14 => "catorce".to_string(),
+                15 => "quince".to_string(),
+                _ => "".to_string(),
+            };
+            
+        } else {
+            unid = get_unidades(num);
+            dec = get_decenas(num);
+
+            result_tupla.0 = match dec {
+                0 => "".to_string(),
+                1 => "dieci".to_string(),
+                2 => "veinte".to_string(),
+                3 => "treinta".to_string(),
+                4 => "cuarenta".to_string(),
+                5 => "cincuenta".to_string(),
+                6 => "sesenta".to_string(),
+                7 => "setenta".to_string(),
+                8 => "ochenta".to_string(),
+                9 => "noventa".to_string(),
+                _ => "".to_string(),
+            };
+
+                if dec != 0 && dec != 1 {
+                    result_tupla.1 = " y ".to_string();
+                } else {
+                    result_tupla.1 = "".to_string();
+                }
+
+            result_tupla.2 = match unid {
+                0 => "cero".to_string(),
+                1 => "uno".to_string(),
+                2 => "dos".to_string(),
+                3 => "tres".to_string(),
+                4 => "cuatro".to_string(),
+                5 => "cinco".to_string(),
+                6 => "seis".to_string(),
+                7 => "siete".to_string(),
+                8 => "ocho".to_string(),
+                9 => "nueve".to_string(),
+                _ => "".to_string(),
+            };
+        }
+
+    println!("\nNúmero\t{} en Letras:\t{}{}{}\n", num, result_tupla.0, result_tupla.1, result_tupla.2);
+    */    
+}
+
+/* 
+fn get_unidades(num: i32) -> i16 {
+    return (num % 10) as i16;
+}
+
+fn get_decenas(num: i32) -> i16 {
+    return (num / 10) as i16;
+}
+*/
+fn get_unidades(num: i32) -> i32 {
+    return num % 10;
+}
+
+fn get_decenas(num: i32) -> i32 {
+    return num / 10;
+}
+
+fn get_num_en_letras_tupla(num: i32) -> (String, String, String) {
     let unid: i32;
     let dec: i32;
     // let unid: i16;
@@ -82,23 +168,5 @@ fn main() {
             };
         }
 
-    println!("\nNúmero\t{} en Letras:\t{}{}{}\n", num, result_tupla.0, result_tupla.1, result_tupla.2);
-
+    return result_tupla;
 }
-/* 
-fn get_unidades(num: i32) -> i16 {
-    return (num % 10) as i16;
-}
-
-fn get_decenas(num: i32) -> i16 {
-    return (num / 10) as i16;
-}
-*/
-fn get_unidades(num: i32) -> i32 {
-    return num % 10;
-}
-
-fn get_decenas(num: i32) -> i32 {
-    return num / 10;
-}
-
