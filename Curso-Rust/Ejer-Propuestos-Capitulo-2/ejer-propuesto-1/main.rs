ab18a0b9a0a804d508cffb7c9faea5180050581f
josejam <joseam.jam.79@gmail.com>
2024-10-01 23:38:37 +0200

    add mod-Ejer-Propuesto-1-V2 Ejer-Propuestos-Capitulo-2

--- Curso-Rust/Ejer-Propuestos-Capitulo-2/ejer-propuesto-1/main.rs
+++ Curso-Rust/Ejer-Propuestos-Capitulo-2/ejer-propuesto-1/main.rs
@@ -27,18 +27,29 @@ fn main() {
     
     //let tupla_letras_dni = ('T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E');
 
-    // let num: i32 = 0;
-    // let num: u8 = 0;
-    let num: u8 = 5;
+    // let num_dni: i32 = 0;
+    // let num_dni: u8 = 0;
+    //let num_dni: u8 = 5;
 
+    let num_dni: u32 = 53274317;
+    let ind: u8 = get_index_letra_dni_u8(num_dni);
+
+    let salida_letra = String::new();
+
+    /*
+    let mut ind: usize = num_dni as usize;
+    
+    let mut salida_letra = String::new();
+    
+    salida_letra = vector_letras_dni[ind].to_string();
+    */ 
+    // salida_letra = array_letras_dni[ind].to_string();     
+            
 
     /*  PRUEBAS   println!("\n{:?}\n", vector_letras_dni[0]);
 
     println!("\n{:?}\n", vector_letras_dni.get(0));
     */
-
-    let num_dni: u32 = 53274317;
-    let ind: u8 = get_index_letra_dni(num_dni);
 
     println!("\nFunctions:\t{}\n", get_letra_dni_vector(ind, vector_letras_dni));
 
@@ -52,20 +63,74 @@ fn main() {
     println!("\nTupla Prueba\t{:?}\n", tupla_letras_dni.0);
     */
 
-    /*  VERSIÓN DIRECTA EN EL MAIN   */
+    /*  VERSIÓN DIRECTA EN EL MAIN  [ FOR  IN ]  */
         for i in 0..array_letras_dni.len() + 1 {
-            if i == num.into() { println!("\n{}\n", array_letras_dni[i]);}
+            if i == ind.into() { println!("\nAsignación Letra DNI\t{} {}\n", num_dni, array_letras_dni[i]);}
         }
+
+    /*  VERSIÓN DIRECTA EN EL MAIN [ LOOP ] */
+        /*loop {
+                if iterador == num_dni { 
+                    
+                    let mut aux: String = String::new();
+                    aux = vector_letras_dni[ind].to_string(); 
+                    // aux = array_letras_dni[ind].to_string();
+
+                    salida_letra = aux;
+
+                    break;
+                } 
+                
+                if num_dni < iterador {
+
+                    let mut aux: String = String::new();
+                    // aux = (vector_letras_dni[0] as str).to_string(); 
+
+                    aux = array_letras_dni[ind].to_string(); 
+                    salida_letra = aux;
+                    
+                    break; 
+                }
+            iterador += 1;
+        }
+        
+        println!("\nAsignación Letra DNI\t{} {}\n", num_dni, salida_letra);
+    */
+
+    /*  VERSIÓN DIRECTA EN EL MAIN [ MATCH ] */
+    /*  //let mut aux_ind = 0..23;
+        let mut aux_ind = 0..vector_string_letras_dni.len();
+        let mut ind: usize = num_dni as usize;
+        let mut salida_letra = String::new();
+
+            loop {
+                match aux_ind.next() { 
+
+                    Some(aux_ind) => { if aux_ind == ind { salida_letra = vector_string_letras_dni[ind]; }   }, 
+                    None => {  salida_letra = "No Imprime nada"; break;     }
+                }
+            }
+
+        println!("\nAsignación Letra DNI\t{} {}\n", num_dni, salida_letra);        
+    */
+    
+
     /*  VERSION A TRAVÉS DE FUNCIÓN PASÁNDOLE UN ARRAY [ CHAR; 23 ]  */    
-    println!("\n{}\n", get_letra_dni_array(num, array_letras_dni));
+    println!("\nAsignación Letra DNI\t{} {}\n", num_dni, get_letra_dni_array(ind, array_letras_dni));
 
+    /*  VERSION DIRECTA EN EL MAIN  [ INDEXACIÓN ] [ ARRAY && VECTORES ] */
+    println!("\nAsignación Letra DNI\t{} {}\n", num_dni, salida_letra);
 }
 
-
-fn get_index_letra_dni(num_dni: u32) -> u8 {
+/*  VERSIÓN DE EXTRACCIÓN DEL INDICE PARA FUNCIONES TIPADAS CON IND: U8 */
+fn get_index_letra_dni_u8(num_dni: u32) -> u8 {
     return (num_dni % 23) as u8;
-    // return num_dni % 23;
 }
+
+/*  VERSIÓN DE EXTRACCIÓN DEL INDICE DEVUELVE IND: USIZE QUE PUEDE USARSE COMO INDICE DIRECTO EN [ ARRAYS && VECTORES ]      */
+// fn get_index_letra_dni_unsize(num_dni: u32) -> usize {
+//     return (num_dni % 23) as usize;
+// }
 
 fn get_letra_dni_array(ind: u8, vec_letras_dni:[char; 23]) -> String {
     let mut result: String = String::new();
@@ -99,3 +164,22 @@ fn get_letra_dni_vector_string(ind: u8, vec_letras_dni: Vec<&str>) -> String {
 
     return result;
 }
+
+/* fn get_letra_dni_match_index_vector_string(ind: u8, vec_letras_dni: Vec<&str>) -> String {
+    let mut result = String::new();
+
+    //let mut aux_ind = 0..23;
+    let mut aux_ind = 0..vector_string_letras_dni.len();
+    let mut ind: usize = num as usize;
+
+        loop {
+            match aux_ind.next() { 
+
+                Some(aux_ind) => { if aux_ind == ind { result = vector_string_letras_dni[ind]; }   }, 
+                None => {  result = "No Imprime nada"; break;     }
+            }
+        }
+
+    return result;
+}
+*/
