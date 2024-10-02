0b392b33e488068528f85a92f64e0e888e141267
josejam <joseam.jam.79@gmail.com>
2024-10-02 17:08:05 +0200

    add mod-Ejer-Propuesto-1-V3 Ejercicios-Propuestos-Capitulo-2

--- Curso-Rust/Ejer-Propuestos-Capitulo-2/ejer-propuesto-1/main.rs
+++ Curso-Rust/Ejer-Propuestos-Capitulo-2/ejer-propuesto-1/main.rs
@@ -16,46 +16,63 @@
 
 use std::io;
 use std::str::FromStr;
+use regex::Regex;
+// use regex;
 
 fn main() {
 
     let vector_letras_dni = vec!['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
-    let vector_string_letras_dni: Vec<&str> = vec!("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E");
+    //let vector_string_letras_dni: Vec<&str> = vec!("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E");
 
-    let array_letras_dni: [char; 23] = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+    //let array_letras_dni: [char; 23] = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
     //let array_string_letras_dni: [&str; 23] = ["T" ,"R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
     
     //let tupla_letras_dni = ('T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E');
 
     // let num_dni: i32 = 0;
-    // let num_dni: u8 = 0;
-    //let num_dni: u8 = 5;
-
-    let num_dni: u32 = 53274317;
-    let ind: u8 = get_index_letra_dni_u8(num_dni);
-
-    let salida_letra = String::new();
-
-    /*
-    let mut ind: usize = num_dni as usize;
     
-    let mut salida_letra = String::new();
+    // let re = Regex::new(r"[0-9]{8}").unwrap();
+    let exp_regex = Regex::new(r"[\d]{8}").unwrap();
+
+    let mut num_dni: u32;
+    // let num_dni: u8;
+
+    // let ind: u8;
+    // let ind: unsize;
+
+        loop {
+
+            println!("\nIntroduce Número de DNI [ 8 Dígitos ]\n");
+            let mut entrada = String::new();
+
+            io::stdin().read_line(&mut entrada).expect("Error en la lectura de datos");
+            num_dni = u32::from_str(&entrada.trim()).unwrap(); 
+
+            // let aux: &str = &num_dni.to_string() as &str;
+
+                if exp_regex.is_match(&num_dni.to_string()) {
+                    break;
+                } else {
+                    println!("\nError en la Introducción de Datos\n");
+                }
+        }
+
+    // let  ind = get_index_letra_dni_u8(num_dni);
+    let ind = get_index_letra_dni_unsize(num_dni);
     
-    salida_letra = vector_letras_dni[ind].to_string();
-    */ 
-    // salida_letra = array_letras_dni[ind].to_string();     
+    // let mut salida_letra = String::new();
+
+    let salida_letra: String = vector_letras_dni[ind].to_string();
+    // salida_letra = array_letras_dni[ind].to_string();  
+    // salida_letra = tupla_letras_dni.ind.to_string(); 
+           
             
-
-    /*  PRUEBAS   println!("\n{:?}\n", vector_letras_dni[0]);
-
-    println!("\n{:?}\n", vector_letras_dni.get(0));
-    */
-
-    println!("\nFunctions:\t{}\n", get_letra_dni_vector(ind, vector_letras_dni));
+    /*  MUESTRA LA SALIDA DE LA FUNCIÓN DIRECTAMENTE EN EL PRINTLN! */
+    // println!("\nFunctions:\t{}\n", get_letra_dni_vector(ind, vector_letras_dni));
 
     /*  MUESTRA LA SALIDA EN FORMA LITERAL [ CHAR || STRING ] */
     // println!("\nFunctions Vector String \t{:?}\n", get_letra_dni_vector_string(ind, vector_string_letras_dni));
-    println!("\nFunctions Vector String \t{}\n", get_letra_dni_vector_string(ind, vector_string_letras_dni));
+    // println!("\nFunctions Vector String \t{}\n", get_letra_dni_vector_string(ind, vector_string_letras_dni));
 
 
     /*  PRUEBAS   println!("\nArray Prueba\t{:?}\n", array_letras_dni[0]);
@@ -64,9 +81,10 @@ fn main() {
     */
 
     /*  VERSIÓN DIRECTA EN EL MAIN  [ FOR  IN ]  */
-        for i in 0..array_letras_dni.len() + 1 {
+        /*for i in 0..array_letras_dni.len() + 1 {
             if i == ind.into() { println!("\nAsignación Letra DNI\t{} {}\n", num_dni, array_letras_dni[i]);}
         }
+        */
 
     /*  VERSIÓN DIRECTA EN EL MAIN [ LOOP ] */
         /*loop {
@@ -116,70 +134,79 @@ fn main() {
     
 
     /*  VERSION A TRAVÉS DE FUNCIÓN PASÁNDOLE UN ARRAY [ CHAR; 23 ]  */    
-    println!("\nAsignación Letra DNI\t{} {}\n", num_dni, get_letra_dni_array(ind, array_letras_dni));
+    // println!("\nAsignación Letra DNI\t{} {}\n", num_dni, get_letra_dni_array(ind, array_letras_dni));
 
     /*  VERSION DIRECTA EN EL MAIN  [ INDEXACIÓN ] [ ARRAY && VECTORES ] */
     println!("\nAsignación Letra DNI\t{} {}\n", num_dni, salida_letra);
 }
 
 /*  VERSIÓN DE EXTRACCIÓN DEL INDICE PARA FUNCIONES TIPADAS CON IND: U8 */
-fn get_index_letra_dni_u8(num_dni: u32) -> u8 {
-    return (num_dni % 23) as u8;
-}
+    /* fn get_index_letra_dni_u8(num_dni: u32) -> u8 {
+        return (num_dni % 23) as u8;
+    }
+    */
 
 /*  VERSIÓN DE EXTRACCIÓN DEL INDICE DEVUELVE IND: USIZE QUE PUEDE USARSE COMO INDICE DIRECTO EN [ ARRAYS && VECTORES ]      */
-// fn get_index_letra_dni_unsize(num_dni: u32) -> usize {
-//     return (num_dni % 23) as usize;
-// }
-
-fn get_letra_dni_array(ind: u8, vec_letras_dni:[char; 23]) -> String {
-    let mut result: String = String::new();
-
-        for i in 0..vec_letras_dni.len() + 1 {
-            // if i == num.into() { println!("\n{}\n", vec_letras_dni[i]);}
-            if i == ind.into() { result =  vec_letras_dni[i].to_string();}
-        }
-
-    return result;
+fn get_index_letra_dni_unsize(num_dni: u32) -> usize {
+    return (num_dni % 23) as usize;
 }
 
-fn get_letra_dni_vector(ind: u8, vec_letras_dni: Vec<char>) -> String {
-    let mut result: String = String::new();
 
-        for i in 0..vec_letras_dni.len() + 1 {
-            // if i == num.into() { println!("\n{}\n", vec_letras_dni[i]);}
-            if i == ind.into() { result =  vec_letras_dni[i].to_string();}
-        }
+/*  VERSION CON UNA ITERACIÓN [ FOR IN ] SOBRE UN ARRAY [ CHAR; 23 ] DEVOLVIENDO UN STRING CON EL CARÁCTER CORRESPONDIENTE     */
+    /* fn get_letra_dni_array(ind: u8, vec_letras_dni: [char; 23]) -> String {
+        let mut result: String = String::new();
 
-    return result;
-}
-
-fn get_letra_dni_vector_string(ind: u8, vec_letras_dni: Vec<&str>) -> String {
-    let mut result: String = String::new();
-
-        for i in 0..vec_letras_dni.len() + 1 {
-            // if i == num.into() { println!("\n{}\n", vec_letras_dni[i]);}
-            if i == ind.into() { result =  vec_letras_dni[i].to_string();}
-        }
-
-    return result;
-}
-
-/* fn get_letra_dni_match_index_vector_string(ind: u8, vec_letras_dni: Vec<&str>) -> String {
-    let mut result = String::new();
-
-    //let mut aux_ind = 0..23;
-    let mut aux_ind = 0..vector_string_letras_dni.len();
-    let mut ind: usize = num as usize;
-
-        loop {
-            match aux_ind.next() { 
-
-                Some(aux_ind) => { if aux_ind == ind { result = vector_string_letras_dni[ind]; }   }, 
-                None => {  result = "No Imprime nada"; break;     }
+            for i in 0..vec_letras_dni.len() + 1 {
+                // if i == num.into() { println!("\n{}\n", vec_letras_dni[i]);}
+                if i == ind.into() { result =  vec_letras_dni[i].to_string();}
             }
-        }
 
-    return result;
-}
-*/
+        return result;
+    }
+    */
+
+/*  VERSION CON UNA ITERACIÓN [ FOR IN ] SOBRE UN VECTOR [ VEC<CHAR> ] DEVOLVIENDO UN STRING CON EL CARÁCTER CORRESPONDIENTE       */
+    /*fn get_letra_dni_vector(ind: u8, vec_letras_dni: Vec<char>) -> String {
+        let mut result: String = String::new();
+
+            for i in 0..vec_letras_dni.len() + 1 {
+                // if i == num.into() { println!("\n{}\n", vec_letras_dni[i]);}
+                if i == ind.into() { result =  vec_letras_dni[i].to_string();}
+            }
+
+        return result;
+    }
+    */
+
+/*  VERSION CON UNA ITERACIÓN [ FOR IN ] SOBRE UN VECTOR [ VEC<&STR> ] [ STRING LITERALES // PASO REFERENCIA ] DEVOLVIENDO UN STRING CON EL CARÁCTER CORRESPONDIENTE       */
+    /*fn get_letra_dni_vector_string(ind: u8, vec_letras_dni: Vec<&str>) -> String {
+        let mut result: String = String::new();
+
+            for i in 0..vec_letras_dni.len() + 1 {
+                // if i == num.into() { println!("\n{}\n", vec_letras_dni[i]);}
+                if i == ind.into() { result =  vec_letras_dni[i].to_string();}
+            }
+
+        return result;
+    }
+    */
+
+/*  VERSION CON UNA ITERACIÓN [ LOOP [ MACH ] ] SOBRE UN VECTOR [ VEC<&STR> ] [ STRING LITERALES // PASO REFERENCIA ] DEVOLVIENDO UN STRING CON EL CARÁCTER CORRESPONDIENTE       */
+    /* fn get_letra_dni_match_index_vector_string(ind: u8, vec_letras_dni: Vec<&str>) -> String {
+        let mut result = String::new();
+
+        //let mut aux_ind = 0..23;
+        let mut aux_ind = 0..vector_string_letras_dni.len();
+        let mut ind: usize = num as usize;
+
+            loop {
+                match aux_ind.next() { 
+
+                    Some(aux_ind) => { if aux_ind == ind { result = vector_string_letras_dni[ind]; }   }, 
+                    None => {  result = "No Imprime nada"; break;     }
+                }
+            }
+
+        return result;
+    }
+    */
