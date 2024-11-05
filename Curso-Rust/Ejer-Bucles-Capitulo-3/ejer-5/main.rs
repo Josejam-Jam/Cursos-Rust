f0f7c66e65e21d14887bd667a786d91c9b87609c
josejam <joseam.jam.79@gmail.com>
2024-11-05 23:18:20 +0100

    add Ejer-5-V1 Ejercicios-Bucles-Capitulo-3

---
+++ Curso-Rust/Ejer-Bucles-Capitulo-3/ejer-5/main.rs
@@ -0,0 +1,112 @@
+/*      Ejer    5       Desarrollar un programa que solicite los valores mínimo y máximo de un rango.
+
+                        A continuación solicitará por teclado un número que debe estar dentro del rango. 
+                        
+                        Si el valor introducido no pertenece al rango, la aplicación volverá a pedir otro valor,
+                        y así repetidas veces, hasta que el valor se encuentre dentro del rango.
+
+*/
+
+use std::io;
+use std::str::FromStr;
+use regex::Regex;
+
+fn main() {
+
+    let mut num_min: i32;
+    let mut num_max: i32;
+    let mut num_user: i32;
+    let exp_regex = Regex::new(r"\d+").unwrap();
+    let exp_regex_no_caracter = Regex::new(r"[a-zA-Z]+").unwrap();
+
+    let mut data_ok: bool;
+
+        loop {
+            println!("\nIntroduce un Número para el Rango Mínimo del Intervalo");
+            let mut entrada_1 = String::new();
+
+            io::stdin().read_line(&mut entrada_1).expect("Error en la lectura de datos");
+
+                if exp_regex_no_caracter.is_match(&entrada_1.trim().to_string()) {
+                    println!("\nError al introducir los Datos Introduzca un número");
+                    
+                } else {
+
+                    num_min = i32::from_str(&entrada_1.trim()).unwrap();
+
+                    data_ok = exp_regex.is_match(&num_min.to_string());
+
+                        // if exp_regex.is_match(&num_min.to_string()) {
+                        if data_ok {
+                            break;
+                        } else {
+                            println!("\nError al introducir los Datos");
+                        }
+                }
+
+        }
+
+        loop {
+            println!("\nIntroduce un Número para el Rango Máximo del Intervalo");
+            let mut entrada_2 = String::new();
+
+            io::stdin().read_line(&mut entrada_2).expect("Error en la lectura de datos");
+
+                if exp_regex_no_caracter.is_match(&entrada_2.trim().to_string()) {
+                    println!("\nError al introducir los Datos Introduzca un número");
+                    
+                } else {
+
+                    num_max = i32::from_str(&entrada_2.trim()).unwrap();
+
+                    data_ok = exp_regex.is_match(&num_max.to_string());
+
+                        // if exp_regex.is_match(&num_max.to_string()) {
+                        if data_ok {
+                            break;
+                        } else {
+                            println!("\nError al introducir los Datos");
+                        }
+
+                }    
+        }
+
+        loop {
+            println!("\nIntroduce un Número que pertenezca al intervalo [ {} - {} ]", num_min, num_max);
+            let mut entrada_3 = String::new();
+
+            io::stdin().read_line(&mut entrada_3).expect("Error en la lectura de datos");
+
+                if exp_regex_no_caracter.is_match(&entrada_3.trim().to_string()) {
+                    println!("\nError al introducir los Datos Introduzca un número");
+                    
+                } else {
+
+                    num_user = i32::from_str(&entrada_3.trim()).unwrap();
+
+                    data_ok = exp_regex.is_match(&num_user.to_string());
+
+                        // if exp_regex.is_match(&num_user.to_string()) && is_range(num_min, num_max, num_user) {
+                        if data_ok && is_range(num_min, num_max, num_user) {
+                            println!("\nEl Número Introducido\t{}\testá dentro del Rango\t[ {} - {} ]\n", num_user, num_min, num_max);
+                            break;
+                        } else {
+                            println!("\nError al introducir los Datos");
+                        }
+
+                }    
+        }
+
+}
+
+fn is_range(num_min: i32, num_max: i32, num: i32) -> bool {
+    let mut result: bool = false;
+
+        if (num >= num_min) && (num <= num_max) { 
+            result = true; 
+        }
+
+    return result;
+
+    // return (num >= num_min) && (num <= num_max);
+}
